bb23943aa2a88d2a2555c4aaadd96867c2c8e94f
Mitra Serat <mitrasirat95@gmail.com>
2023-04-16 10:32:41 -0700

    Added Clear button and Equal button

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 let screen = document.querySelector(".screen");
 let buttons = document.querySelectorAll(".btn");
-
+let cleaqr = document.querySelector(".btn-clear");
+let equal = document.querySelector(".btn-equal");
 
 })()
